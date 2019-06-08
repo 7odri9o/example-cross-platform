6ca2c37ade7ef643ed4a3f77f1ac40d82836a72d
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2019-06-08 13:13:12 -0300

    Adding another commit

--- test/unit/unit.spec.js
+++ test/unit/unit.spec.js
@@ -2,3 +2,9 @@ test('adds 1 + 2 to equal 3', () => {
   const number = 3
   expect(number).toBe(3);
 });
+
+test('adds 1 + 2 to equal 3', () => {
+  const number = 3
+  expect(number).toBe(3);
+});
+
